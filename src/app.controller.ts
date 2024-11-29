0f06e5ee966d9e042911e405db7c2319b796f7f2
santiago sierra <santiscano@gmail.com>
2024-11-29 10:55:39 -0500

    add log

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly logger = new Logger(AppController.name);
+
+  constructor(
+    private readonly appService: AppService,
+  ) {}
 
   @Get('/test')
   getHello(): Record<string, string> {
+    this.logger.log('Hello World!');
     return this.appService.getHello();
   }
 }
